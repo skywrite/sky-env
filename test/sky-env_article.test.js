144b9b2fea110ef4aea3fdc4eed3a890d310411c
JP Richardson <jprichardson@gmail.com>
2013-07-18 17:31:05 -0500

    moved some tests around

--- test/sky-env_article.test.js
+++ test/sky-env_article.test.js
@@ -38,5 +38,47 @@ describe('SkyEnv', function() {
         })
       })
     })
+
+    describe('- mdArticleToOutputFileWithPath(mdfile, [data])', function() {
+      describe('> when no data is passed as an arg', function() {
+        it('should return a file with slug based upon markdown file name', function() {
+          var se = new SkyEnv('/tmp')
+          se.configs = {get: function() { return ''}} //mock it up
+          EQ (se.mdArticleToOutputFileWithPath('/tmp/articles/bitcoin-economics.md'), '/tmp/public/bitcoin-economics.html')
+        })
+      })
+
+      describe('> when urlformat exists', function() {
+        it('should return the proper name with the data slug', function() {
+          var articlesDir = path.join(TEST_DIR, 'articles')
+          fs.writeJsonSync(CFG_FILE, {articles: {urlformat: '{{slug}}'}})
+          
+          var se = new SkyEnv(tl.findBaseDirSync())
+          se.loadConfigsSync()
+          var data = {
+            slug: 'burt-and-ernie'
+          }
+          EQ (se.mdArticleToOutputFileWithPath(path.join(articlesDir, 'bitcoin-economics.md'), data), path.join(se.outputDir, data.slug))
+        })
+      })
+
+      describe('> when urlformat does not exist', function() {
+        it('should return the proper name with the data slug', function() {
+          var articlesDir = path.join(TEST_DIR, 'articles')
+          fs.writeJsonSync(CFG_FILE, {})
+          
+          var se = new SkyEnv(tl.findBaseDirSync())
+          se.loadConfigsSync()
+          var data = {
+            slug: 'burt-and-ernie',
+            'date-year': '2011',
+            'date-month': '04'
+          }
+
+          var expected = "articles/2011/04/burt-and-ernie.html"
+          EQ (se.mdArticleToOutputFileWithPath(path.join(articlesDir, 'bitcoin-economics.md'), data), path.join(se.outputDir, expected))
+        })
+      })
+    })
   })
 })
